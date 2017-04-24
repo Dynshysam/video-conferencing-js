42978b42e12d43c9db4e8428c68eaa57fb47dc52
Ormat <noemail@example.com>
2017-04-24 17:57:07 +1200

    Video conferencing basic functionality

--- server/server.js
+++ server/server.js
@@ -1,10 +1,66 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const HTTPS_PORT = 8443;
+
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+// Yes, SSL is required
+const serverConfig = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+};
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for the client html page
+var handleRequest = function(request, response) {
+    // Render the single client html file for any request the HTTP server receives
+    console.log('request received: ' + request.url);
+
+    if(request.url === '/') {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(fs.readFileSync('client/index.html'));
+    } else if(request.url === '/app.js') {
+        response.writeHead(200, {'Content-Type': 'application/javascript'});
+        response.end(fs.readFileSync('client/app.js'));
+    }
+};
+
+var httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT);
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for handling websocket calls
+var wss = new WebSocketServer({server: httpsServer});
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        // Broadcast any received message to all clients
+        console.log('received: %s', message);
+        wss.broadcast(message);
+    });
+});
+
+wss.broadcast = function(data) {
+    this.clients.forEach(function(client) {
+        if(client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+console.log('Server running. Visit https://localhost:' + HTTPS_PORT + ' in Firefox/Chrome (note the HTTPS; there is no HTTP -> HTTPS redirect!)');
 
 
-var server = http.createServer(function(request, response) {
-  if (request.url === "/") {
-    
-  }
-}).listen(8080);
+
+// var http = require('http');
+
+
+// var server = http.createServer(function(request, response) {
+//   if (request.url === "/") {
+//     response.write("Hello Word");
+//     response.end();
+//   }
+// }).listen(8080);
